0048eed490a65a0757b8c4a8ebce3bcc256bbbed
SunyiUborka <csehi.rolcsi@gmail.com>
2022-10-19 14:32:25 +0200

    update

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on('interactionCreate', async interaction => {
     if (!command) return;
 
     try {
-        await command.execute(interaction);
+        await command.execute(interaction, client);
     } catch (error) {
         console.error(error);
         await interaction.reply({ content: 'Azt hiszem baj van!', ephemeral: true });
