8643b31171e67b5ec5abee4574fe9998529737a0
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2022-10-24 22:12:27 +0200

    Delete interactionCreate.js

--- events/interactionCreate.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'interactionCreate',
-    execute(interaction) {
-        console.log(`${interaction.user.tag} felhasznált egy parancsot #${interaction.channel.name}-ban/ben`);
-        },
-};
