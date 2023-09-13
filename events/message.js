ae6bebf6e51e8b11360954117a0bb554d2548164
SunyiUborka <csehi.rolcsi@gmail.com>
2023-09-13 20:19:23 +0200

    add hhh reaction

--- events/message.js
+++ events/message.js
@@ -3,8 +3,11 @@ import {Events, AttachmentBuilder} from "discord.js";
 export default {
     name: Events.MessageCreate,
     async execute(message) {
+        console.log(message.content);
         if(message.author.bot) return;
         if (message.content.includes(`sex`) || message.content.includes(`szex`)) 
-        await message.channel.send(`https://cdn.discordapp.com/emojis/1105482456250974210.gif?size=160&quality=lossless`)
+            await message.channel.send(`https://cdn.discordapp.com/emojis/1105482456250974210.gif?size=160&quality=lossless`)
+        if (message.content.includes(`<a:Hhh:1105482456250974210>`))
+            await message.channel.send(`https://cdn.discordapp.com/emojis/1105516399318806538.webp?size=160&quality=lossless`)
     },
 };
