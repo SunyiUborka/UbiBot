cdb8e5dd61e402ba105ab3ee9ad5b223ed48cbc0
SunyiUborka <csehi.rolcsi@gmail.com>
2023-09-13 20:22:06 +0200

    remove log

--- events/message.js
+++ events/message.js
@@ -3,7 +3,6 @@ import {Events, AttachmentBuilder} from "discord.js";
 export default {
     name: Events.MessageCreate,
     async execute(message) {
-        console.log(message.content);
         if(message.author.bot) return;
         if (message.content.includes(`sex`) || message.content.includes(`szex`)) 
             await message.channel.send(`https://cdn.discordapp.com/emojis/1105482456250974210.gif?size=160&quality=lossless`)
