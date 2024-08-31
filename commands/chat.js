87ac85997223355efe30ede955de8de47fa65156
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2024-09-01 01:33:52 +0200

    updated to latest package

--- commands/chat.js
+++ commands/chat.js
@@ -1,44 +1,45 @@
-import { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } from 'discord.js'
-import { Configuration, OpenAIApi } from 'openai'
-import { setTimeout } from 'node:timers/promises'
-import dotenv from 'dotenv'
+import {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  EmbedBuilder,
+} from "discord.js";
+import { OpenAI } from "openai";
+import { setTimeout } from "node:timers/promises";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const conf = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-})
-const openai = new OpenAIApi(conf);
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export default {
-    data: new SlashCommandBuilder()
-    .setName('chat')
-    .setDescription('Chat with OpenAI.')
-    .addStringOption(option => option
-    .setName('input')
-    .setDescription('Ask a question to AI.')
-    .setRequired(true))
+  data: new SlashCommandBuilder()
+    .setName("chat")
+    .setDescription("Chat with OpenAI.")
+    .addStringOption((option) =>
+      option
+        .setName("input")
+        .setDescription("Ask a question to AI.")
+        .setRequired(true),
+    )
     .setDMPermission(true)
     .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
-    async execute(interaction) {
-        interaction.deferReply();
+  async execute(interaction) {
+    await interaction.deferReply();
 
-        const inputString = interaction.options.getString('input');
+    const inputString = interaction.options.getString("input");
 
-        const response = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: `${inputString}`,
-            max_tokens: 1024,
-            temperature: 0.9
-        });
+    const chat = await openai.chat.completions.create({
+      messages: [{ role: "user", content: inputString }],
+      model: "gpt-3.5-turbo",
+    });
 
-        const embed = new EmbedBuilder()
-        .setTitle(`\ ${inputString}`)
-        .setDescription(`\ ${response.data.choices[0].text}`)
-        .setColor('Random');
+    const embed = new EmbedBuilder()
+      .setTitle(`\ ${inputString}`)
+      .setDescription(`\ ${chat.choices[0].message.content}`)
+      .setColor("Random");
 
-        await setTimeout(15000)
-        await interaction.editReply({ embeds: [embed]})
-
-        },
+    await interaction.editReply({ embeds: [embed] });
+  },
 };
